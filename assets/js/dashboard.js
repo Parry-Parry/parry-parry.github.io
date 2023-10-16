db33f11127b28761b9e39c9596188145b1e59af1
Andrew Parry <2393265p@student.gla.ac.uk>
2023-10-16 12:58:29 +0100

    praying this works

---
+++ assets/js/dashboard.js
@@ -0,0 +1,80 @@
+const repoOwner = 'Parry-Parry';
+const repoName = 'workstationtracker';
+
+// Function to fetch log data from a specific file in the repository
+async function fetchLogData(systemID) {
+    const response = await fetch(`https://raw.githubusercontent.com/${repoOwner}/${repoName}/main/logs/${systemID}.log`);
+    return await response.text();
+}
+
+// Function to parse log data and extract the most recent utilization
+function parseLogData(logData, systemID) {
+    if (!logData.trim()) {
+        // Handle empty log files
+        return [{ systemID, error: 'No data available' }];
+    }
+
+    // Split log data by lines
+    const lines = logData.split('\n');
+    
+    // Create an object to store the most recent utilization for each GPU
+    const gpuUtilizations = {};
+
+    // Process each line of the log data
+    for (const line of lines) {
+        const [datetime, gpuID, utilization] = line.split('\t');
+        
+        if (gpuID && utilization) {
+            const gpuIDKey = `GPU${gpuID}`;
+            gpuUtilizations[gpuIDKey] = {
+                datetime,
+                utilization: parseInt(utilization)
+            };
+        }
+    }
+
+    // Find the most recent utilization for each GPU
+    const mostRecentUtilizations = Object.values(gpuUtilizations);
+
+    mostRecentUtilizations.sort((a, b) => a.datetime.localeCompare(b.datetime));
+
+    return mostRecentUtilizations;
+}
+
+// Function to populate the table with data
+async function populateTable() {
+    const systemIDs = ['uama', 'bruce', 'neoch', 'baird']; // Add your system IDs here
+
+    for (const systemID of systemIDs) {
+        const logData = await fetchLogData(systemID);
+        const utilizationData = parseLogData(logData, systemID);
+
+        for (const data of utilizationData) {
+            const row = document.createElement('tr');
+            const systemIDCell = document.createElement('td');
+            const gpuIDCell = document.createElement('td');
+            const utilizationCell = document.createElement('td');
+
+            systemIDCell.textContent = data.systemID;
+            if (data.error) {
+                gpuIDCell.textContent = 'Error';
+                utilizationCell.textContent = data.error;
+            } else {
+                gpuIDCell.textContent = data.gpuID;
+                utilizationCell.textContent = `${data.utilization}%`;
+            }
+            row.appendChild(systemIDCell);
+            row.appendChild(gpuIDCell);
+            row.appendChild(utilizationCell);
+
+            document.getElementById('data-table').appendChild(row);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    // This code will execute when the page is fully loaded.
+
+    // Call the populateTable() function here.
+    populateTable();
+});
---
+++ dashboard.md
@@ -0,0 +1,21 @@
+---
+layout: page
+title: Dashboard
+---
+
+<div class="container">
+    <h1>Workstation Usage</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>System ID</th>
+                <th>GPU ID</th>
+                <th>Most Recent Utilization</th>
+            </tr>
+        </thead>
+        <tbody id="data-table">
+        </tbody>
+    </table>
+</div>
+
+<script src="{{ 'assets/dashboard.js' | relative_url }}"></script>
