188321522265e2e65f30ca525f2cb46b2f749486
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-21 23:44:08 +0000

    maybe?

--- assets/js/background.js
+++ assets/js/background.js
@@ -1,62 +1,88 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const canvas = document.getElementById('canvas');
-    const ctx = canvas.getContext('2d');
+let mouseX = 0;
+let mouseY = 0;
 
-    // Set canvas dimensions
+// Canvas and animation
+let canvas;
+let ctx;
+const animation = () => {
+
+  // Clear canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  
+  // Draw background grid
+  drawGrid();
+  
+  // Perturb grid
+  perturbGrid();
+
+  requestAnimationFrame(animation);
+}
+function resizeCanvas() {
     canvas.width = window.innerWidth;
-    canvas.height = window.innerHeight;
+    canvas.height = window.innerHeight; 
+  }
+  function drawGrid() {
 
-    // Draw grid
-    const gridSize = 50;
-    ctx.lineWidth = 0.1;
-    ctx.strokeStyle = 'white';
-
-    for(let i=0; i<canvas.width; i+=gridSize) {
+    // Draw white grid lines
+    ctx.strokeStyle = 'rgba(255,255,255, 0.1)';
+    
     ctx.beginPath();
-    ctx.moveTo(i, 0);
-    ctx.lineTo(i, canvas.height);
+    
+    for(let i=0; i<canvas.width; i+=50) {
+      ctx.moveTo(i, 0);
+      ctx.lineTo(i, canvas.height);
+    }
+    
+    for(let j=0; j<canvas.height; j+=50) {
+      ctx.moveTo(0, j);
+      ctx.lineTo(canvas.width, j);
+    }
+    
     ctx.stroke();  
-    }
-
-    for(let j=0; j<canvas.height; j+=gridSize) {
-    ctx.beginPath();  
-    ctx.moveTo(0, j);
-    ctx.lineTo(canvas.width, j);
-    ctx.stroke();
-    }
-
-    // Track mouse position
-    let mouseX = 0;
-    let mouseY = 0;
-
-    window.addEventListener('mousemove', e => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;  
-    });
-
-    // Perturb grid
-    function perturbGrid() {
+  }
+  
+  // Perturb grid
+  function perturbGrid() {
+   
+    // Draw perturbation circles  
+    ctx.fillStyle = 'rgba(0,0,0,0.07)';
     
-    ctx.fillStyle = 'rgba(0,0,0,0.1)';
-    
-    // Draw perturbation circles
-    for(let i=0; i<canvas.width; i+=gridSize) {
-        for(let j=0; j<canvas.height; j+=gridSize) {
-        
-        const dx = i - parseFloat(mouseX);
-        const dy = j - parseFloat(mouseY);
-        const dist = Math.sqrt(dx*dx + dy*dy);
-        
-        const pert =  Math.abs(Math.sin(Math.abs(dist)/20))*20;
+    for(let i=0; i<canvas.width; i+=50) {
+      for(let j=0; j<canvas.height; j+=50) {
+      
+        let dx = i - mouseX;
+        let dy = j - mouseY;
+        let dist = Math.sqrt(dx*dx + dy*dy);
+      
+        let pert = Math.abs(Math.sin(dist/30))*30;
         
         ctx.beginPath();
         ctx.arc(i, j, pert, 0, Math.PI*2);
         ctx.fill();
-        }
-    }  
-    
-    requestAnimationFrame(perturbGrid);
-    } 
-    perturbGrid();
-  });
-  window.addEventListener('resize', perturbGrid);
+      }
+    }
+  }
+  
+// Initialize 
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get canvas
+    canvas = document.getElementById('canvas');
+    ctx = canvas.getContext('2d');
+
+    // Set size to window
+    resizeCanvas();
+
+    // Start animation
+    animation();
+
+    // Mouse Move listener  
+    window.addEventListener('mousemove', (event) => {
+        mouseX = event.clientX;  
+        mouseY = event.clientY;
+    });
+
+    // Resize listener
+    window.addEventListener('resize', resizeCanvas);
+
+});
