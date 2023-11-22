58c02a9e60ef07a747bf3168e403266b60a47ffd
Andrew Parry <2393265p@student.gla.ac.uk>
2023-11-22 17:14:35 +0000

    so silly

--- assets/js/gaussian.js
+++ assets/js/gaussian.js
@@ -1,5 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const canvas = document.getElementById('backgroundCanvas');
+    const canvas = document.getElementById('canvas');
     const ctx = canvas.getContext('2d');
 
     let mouseX = 0;
